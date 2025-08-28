d2b7ee6f830e98fa77471a8c296e517ff70c52fc
davidvargasvg-gif <david.vargas.vg@gmail.com>
2025-08-28 17:25:59 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const launchDate = new Date("December 31, 2025 23:59:59").getTime();
+
+const countdown = setInterval(function() {
+    const now = new Date().getTime();
+    const distance = launchDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    if (distance < 0) {
+        clearInterval(countdown);
+        document.querySelector(".countdown-timer").innerHTML = "<h2>¡Hemos Lanzado!</h2>";
+    }
+}, 1000);
